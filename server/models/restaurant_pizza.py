5de1c203d3516d798a9f7ef255f070fa779a15b4
Rodney <rodney.amani@student.moringaschool.com>
2025-06-18 08:49:48 +0300

    changes in resturant_pizza.py

--- server/models/restaurant_pizza.py
+++ server/models/restaurant_pizza.py
@@ -1,16 +1,20 @@
-from server.config import db
-from sqlalchemy.orm import validates
+from server.app import db
 
 class RestaurantPizza(db.Model):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
 
-    @validates('price')
-    def validate_price(self, key, price):
-        if price < 1 or price > 30:
-            raise ValueError("Price must be between 1 and 30")
-        return price
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+
+    # Custom validator: price must be between 1 and 30
+    def validate(self):
+        errors = []
+        if not (1 <= self.price <= 30):
+            errors.append("Price must be between 1 and 30")
+        return errors
+
+    def __repr__(self):
+        return f'<RestaurantPizza ${self.price}>'
